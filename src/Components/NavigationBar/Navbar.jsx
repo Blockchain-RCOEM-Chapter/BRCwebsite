7b8ac08e12a96a8e57529cbd48fe400a6e8fbe6e
aniketraut16 <rautan_1@rknec.edu>
2023-11-10 13:55:46 +0530

    Updated Navigation Bar As per New Design

--- src/Components/NavigationBar/Navbar.jsx
+++ src/Components/NavigationBar/Navbar.jsx
@@ -1,75 +1,119 @@
 import React, { useState } from "react";
 import "./Navbar.css";
+
 function Navbar() {
-  const [isBurgerActive, setisBurgerActive] = useState(
-    window.innerWidth > 500 ? true : false
-  );
-  const toggleHamberger = () => {
-    if (isBurgerActive) {
-      setisBurgerActive(false);
-    } else {
-      setisBurgerActive(true);
-    }
-  };
+  // To Manage State when Side Navbar is open or not
+  const [isBurgerActive, setisBurgerActive] = useState(false);
+
+  // For Side Navbar opening animation
+  let counter = 0;
+
+  //To find whether the device is Phone or not
+  const isPhone = window.innerWidth < 480 ? true : false;
+
   return (
     <nav>
+      {/* It will Conatain  logo */}
       <div className="logo"></div>
-      <div className="right-side">
-        <ul style={isBurgerActive ? { right: "0" } : { right: "-52%" }}>
-          <li className="nav-element">
-            <i
-              className="fa-solid fa-address-book"
-              style={{ color: "#ffffff" }}
-            ></i>
-            CONTACT US
-          </li>
-          <li className="nav-element">
-            <i
-              className="fa-solid fa-people-group"
-              style={{ color: "#ffffff" }}
-            ></i>
-            TEAM
-          </li>
-          <li className="nav-element">
-            <i
-              className="fa-solid fa-calendar-check"
-              style={{ color: "#ffffff" }}
-            ></i>
-            EVENTS
-          </li>
-          <li className="nav-element">
-            <i className="fa-solid fa-house" style={{ color: "#ffffff" }}></i>
-            HOME
-          </li>
-        </ul>
 
-        <div className="tagline">BRC RCOEM | COMMUNITY OF HUSTLERS!</div>
+      {/* It will contain the BRC text  */}
+      <div className="brc">
+        BLOCKCHAIN <span> RCOEM CHAPTER</span>
       </div>
-      <div className="burger" onClick={toggleHamberger}>
+
+      {/* It will conatin Navigation elements */}
+      <ul
+        style={
+          isPhone ? (isBurgerActive ? { right: "0px" } : { right: "-62%" }) : {}
+        }
+      >
+        <li
+          style={
+            isPhone
+              ? isBurgerActive
+                ? { transform: `translateX(0%)` }
+                : { transform: `translateX(${100 * ++counter}%)` }
+              : {}
+          }
+        >
+          <i className="fa-solid fa-house" style={{ color: "#ffffff" }}></i>HOME
+        </li>
+        <li
+          style={
+            isPhone
+              ? isBurgerActive
+                ? { transform: `translateX(0%)` }
+                : { transform: `translateX(${100 * ++counter}%)` }
+              : {}
+          }
+        >
+          <i
+            className="fa-solid fa-calendar-check"
+            style={{ color: "#ffffff" }}
+          ></i>
+          EVENTS
+        </li>
+        <li
+          style={
+            isPhone
+              ? isBurgerActive
+                ? { transform: `translateX(0%)` }
+                : { transform: `translateX(${100 * ++counter}%)` }
+              : {}
+          }
+        >
+          <i
+            className="fa-solid fa-people-group"
+            style={{ color: "#ffffff" }}
+          ></i>
+          TEAM
+        </li>
+        <li
+          style={
+            isPhone
+              ? isBurgerActive
+                ? { transform: `translateX(0%)` }
+                : { transform: `translateX(${100 * ++counter}%)` }
+              : {}
+          }
+        >
+          <i
+            className="fa-solid fa-address-book"
+            style={{ color: "#ffffff" }}
+          ></i>
+          CONTACT US
+        </li>
+      </ul>
+
+      {/* Burger to open Side Navbar when devive is Phone */}
+      <div
+        className="burger"
+        onClick={() => {
+          setisBurgerActive(!isBurgerActive);
+        }}
+      >
         <div
           className="top"
           style={
             isBurgerActive
-              ? { transform: "rotate(45deg) translateY(700%)" }
-              : {}
+              ? { transform: "rotate(45deg) translateY(8px)" }
+              : { transform: "rotate(0deg) translateY(0%)" }
           }
         ></div>
         <div
           className="middle"
           style={
             isBurgerActive
-              ? {
-                  transform: "rotate(45deg) translateX(1000%)",
-                }
-              : {}
+              ? { transform: "translateX(500%)" }
+              : { transform: "translateX(0%)" }
           }
         ></div>
         <div
           className="bottom"
           style={
             isBurgerActive
-              ? { transform: "rotate(-45deg) translateY(-700%)" }
-              : {}
+              ? { transform: "rotate(-45deg) translateY(-8px)" }
+              : { transform: "rotate(0deg) translateY(0%)" }
           }
         ></div>
       </div>
