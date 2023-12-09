79ea500a73a0d68151df848b4655cfa7d055ba10
Aniket Raut <138971524+aniketraut16@users.noreply.github.com>
2023-12-09 23:22:13 +0530

    Update Team.jsx

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -8,7 +8,7 @@ function Team() {
       top: 0,
       behavior: "smooth",
     });
-    document.title = "BRC | Events";
+    document.title = "BRC | Team";
   }, []);
 
   const [domain, setdomain] = useState("techTeamMembers");
