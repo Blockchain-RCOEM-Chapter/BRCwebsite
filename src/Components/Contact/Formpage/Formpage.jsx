77a6118cd77328a70e659a6bfaf4775632bf59ce
aniketraut16 <rautan_1@rknec.edu>
2023-12-10 19:53:34 +0530

    Added Emailjs To Contact Page

--- src/Components/Contact/Formpage/Formpage.jsx
+++ src/Components/Contact/Formpage/Formpage.jsx
@@ -1,96 +1,54 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./Formpage.css";
-import emailjs from "emailjs-com";
-
 
 function Formpage() {
-  const [formData, setFormData] = useState({
+  const form = useRef();
+  const emailService = process.env.REACT_APP_EMAIL_SERVICE_ID;
+  const templateId = process.env.REACT_APP_TEMPLATE_ID;
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
-    name: "",
-    email: "",
-    message: "",
-  });
-  
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    try {
-      // Replace these values with your Email.js configuration
-      const emailService = "service_dmwsyvi";
-      const templateId = "template_tsnevom";
-      const userId = "tPtCvk0eYyLAUrNQT";
-  
-      const templateParams = {
-        name: formData.name,
-        email: formData.email,
-        message: formData.message,
-      };
-  
-      await emailjs.send(emailService, templateId, templateParams, userId);
-  
-      console.log("Email sent successfully!");
-    } catch (error) {
-      console.error("Error sending email:", error);
-    }
-    // You can handle form submission logic here
-    console.log("Form data submitted:", formData);
+
+    emailjs.sendForm(emailService, templateId, form.current, publicKey).then(
+      (result) => {
+        console.log(result.text);
+        alert("Message sent successfully!");
+        form.current.reset();
+      },
+      (error) => {
+        alert("Message Not Send, Retry after Sometime!!");
+        console.log(error.text);
+      }
+    );
   };
 
   return (
-    
     <div className="Form">
-      <form onSubmit={handleSubmit}>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="contatusform-name">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+          <label>Name</label>
+          <input type="text" name="to_name" required />
         </div>
         <div className="contactusform-email">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
+          <label>Email</label>
+          <input type="email" name="from_name" required />
+        </div>
+        <div className="contactform-msg">
+          <label>Message</label>
+          <textarea
+            name="message"
+            placeholder="Type something here!"
             required
           />
-        </div>
-
-        <div className="contactform-msg">
-          <label htmlFor="message">Message:</label>
-          <textarea
-            placeholder="Type something here!"
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
         </div>
         <button type="submit" className="submit-btn">
           Submit
         </button>
       </form>
     </div>
-   
   );
 }
 
 export default Formpage;
-
-
